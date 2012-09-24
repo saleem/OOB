30b969bd4d3ff0b0d6746633f845f67965804068
Saleem Siddiqui <siddiqui.saleem@gmail.com>
2012-09-24 18:51:07 -0500

    [saleem] Added tests and code for creating Squares, computing their area, and comparing Squares and Rectangles

--- src/com/saleem_siddiqui/oob/shapes/Rectangle.java
+++ src/com/saleem_siddiqui/oob/shapes/Rectangle.java
@@ -7,12 +7,40 @@ public class Rectangle {
     private final int length;
     private final int width;
 
-    public Rectangle(int length, int width) {
+    private Rectangle(int length, int width) {
         this.length = length;
         this.width = width;
     }
 
+    public static Rectangle createRectangle(int length, int width) {
+        return new Rectangle(length, width);
+    }
+
+    public static Rectangle createSquare(int side) {
+        return new Rectangle(side, side);
+    }
+
     public int area() {
         return length * width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Rectangle rectangle = (Rectangle) o;
+
+        if (length != rectangle.length) return false;
+        if (width != rectangle.width) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = length;
+        result = 31 * result + width;
+        return result;
     }
 }
--- test/com/saleem_siddiqui/oob/shapes/TestRectangle.java
+++ test/com/saleem_siddiqui/oob/shapes/TestRectangle.java
@@ -2,6 +2,9 @@ package com.saleem_siddiqui.oob.shapes;
 
 import org.junit.Test;
 
+import static com.saleem_siddiqui.oob.shapes.Rectangle.createRectangle;
+import static com.saleem_siddiqui.oob.shapes.Rectangle.createSquare;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,6 +14,23 @@ import static org.junit.Assert.assertThat;
 public class TestRectangle {
     @Test
     public void shouldComputeAreaOfRectangle() {
-        assertThat(new Rectangle(4, 5).area(), is(20));
+        assertThat(createRectangle(4, 5).area(), is(20));
+        assertThat(createRectangle(6, 7).area(), is(42));
+    }
+
+    @Test
+    public void shouldComputeAreaOfSquare() {
+        assertThat(createSquare(4).area(), is(16));
+        assertThat(createSquare(6).area(), is(36));
+    }
+
+    @Test
+    public void twoRectanglesWithSameDimensionsShouldBeEqual() {
+        assertThat(createRectangle(4, 5), equalTo(createRectangle(4, 5)));
+    }
+
+    @Test
+    public void rectangleAndSquareWithSameDimensionsShouldBeEqual() {
+        assertThat(createRectangle(5, 5), equalTo(createSquare(5)));
     }
 }
